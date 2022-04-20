20c9b7762215b75c228245c74b148d202f724e71
wilian gonçalves <wwiliangoncalves@gmail.com>
2022-04-20 15:31:16 -0300

    arrumado o user and profile

--- config/database.mjs
+++ config/database.mjs
@@ -1,14 +1,10 @@
 import mysql from "mysql2";
 
 const connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "teste123",
+    host: "db4free.net",
+    user: "wile9090",
+    password: "Swordft10!",
     database: "betasocial"
-    // host: "db4free.net",
-    // user: "wile9090",
-    // password: "Swordft10!",
-    // database: "betasocial"
 });
 
 export default connection;
