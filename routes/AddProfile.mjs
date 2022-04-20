5ee34aa148a81ef5788e0dd5545b507f40c9d149
wilian gonçalves <wwiliangoncalves@gmail.com>
2022-04-20 14:34:11 -0300

    go

--- routes/AddProfile.mjs
+++ routes/AddProfile.mjs
@@ -19,7 +19,7 @@ Router.post("/", (req, res) => {
         // se tudo estiver ok, salva no request para uso posterior.
         req.userId = decoded.id;
 
-        database.query(`INSERT INTO users WHERE id = '${req.userId}'`, (err, result) => {
+        database.query(`INSERT INTO user, profile FROM users WHERE id = '${req.userId}'`, (err, result) => {
             if(err){
                 console.log("Não foi achado nenhum usuário Profile.mjs!");
             }
