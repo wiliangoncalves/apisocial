1001f68bb77e55d4af44f948ade879d2e1741827
wilian gonçalves <wwiliangoncalves@gmail.com>
2022-05-16 11:55:55 -0300

    go

--- config/database.mjs
+++ config/database.mjs
@@ -6,10 +6,15 @@ const connection = mysql.createConnection({
     // password: "teste123",
     // database: "betasocial"
 
-    host: "db4free.net",
-    user: "wile9090",
-    password: "Swordft10!",
-    database: "betasocial"
+    // host: "db4free.net",
+    // user: "wile9090",
+    // password: "Swordft10!",
+    // database: "betasocial"
+
+    host: "bg9cascg4pxlrm8gtj7b-mysql.services.clever-cloud.com",
+    user: "un3bxmnlyvxewy8g",
+    password: "Regcb3fw8BBFH7A405Gc",
+    database: "bg9cascg4pxlrm8gtj7b"
 });
 
 export default connection;
