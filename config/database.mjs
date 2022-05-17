0501e1163919d232820cd9be755f6721b4b62e6a
wilian gonçalves <wwiliangoncalves@gmail.com>
2022-05-17 15:33:05 -0300

    go

--- config/database.mjs
+++ config/database.mjs
@@ -14,7 +14,8 @@ const connection = mysql.createConnection({
     host: "bg9cascg4pxlrm8gtj7b-mysql.services.clever-cloud.com",
     user: "un3bxmnlyvxewy8g",
     password: "Regcb3fw8BBFH7A405Gc",
-    database: "bg9cascg4pxlrm8gtj7b"
+    database: "bg9cascg4pxlrm8gtj7b",
+    uri: "mysql://un3bxmnlyvxewy8g:Regcb3fw8BBFH7A405Gc@bg9cascg4pxlrm8gtj7b-mysql.services.clever-cloud.com:3306/bg9cascg4pxlrm8gtj7b"
 });
 
 export default connection;
