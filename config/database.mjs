4f12079b6b68ba7a304bdc59cb21d51eb159d4aa
black <hat@pop-os.localdomain>
2022-06-27 14:53:03 -0300

    go

--- config/database.mjs
+++ config/database.mjs
@@ -1,16 +1,16 @@
-iimport mysql from "mysql2";
+import mysql from "mysql2";
 
 const connection = mysql.createConnection({
-    // host: "localhost",
-    // user: "root",
-    // password: "teste123",
-    // database: "betasocial"
+    host: "localhost",
+    user: "root",
+    password: "teste123",
+    database: "betasocial"
 
-    host: "bg9cascg4pxlrm8gtj7b-mysql.services.clever-cloud.com",
-    user: "un3bxmnlyvxewy8g",
-    password: "Regcb3fw8BBFH7A405Gc",
-    database: "bg9cascg4pxlrm8gtj7b",
-    uri: "mysql://un3bxmnlyvxewy8g:Regcb3fw8BBFH7A405Gc@bg9cascg4pxlrm8gtj7b-mysql.services.clever-cloud.com:3306/bg9cascg4pxlrm8gtj7b"
+    // host: "bg9cascg4pxlrm8gtj7b-mysql.services.clever-cloud.com",
+    // user: "un3bxmnlyvxewy8g",
+    // password: "Regcb3fw8BBFH7A405Gc",
+    // database: "bg9cascg4pxlrm8gtj7b",
+    // uri: "mysql://un3bxmnlyvxewy8g:Regcb3fw8BBFH7A405Gc@bg9cascg4pxlrm8gtj7b-mysql.services.clever-cloud.com:3306/bg9cascg4pxlrm8gtj7b"
 });
 
 export default connection;
